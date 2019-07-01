fd37467f923256fa56e436bb59142b16fa25edac
humwawe <wawe8963098@163.com>
2019-07-01 22:51:16 +0800

    dp

---
+++ src/main/java/template/dp/Dp.java
@@ -0,0 +1,24 @@
+package template.dp;
+
+/**
+ * the time u need think of dp
+ * a) maximum or minimum
+ * b) yes or no
+ * c) sum
+ * can not sort/swap
+ * <p>
+ * four typical type
+ * 1. matrix dp (10%) dp[row][col]
+ * 2. sequence (40%) dp[pos]
+ * 3. two sequence dp (40%) dp[posI][posJ]
+ * 4. backpack (10%) dp[pos][v]
+ * <p>
+ * when can not solve, think about add dimension
+ * if want to record the deduction process, think about add another array
+ * <p>
+ * key word: state, transfer function, initialization, final answer
+ *
+ * @author hum
+ */
+public class Dp {
+}
