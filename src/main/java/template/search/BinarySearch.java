a64ac9f3367e164eb16f927f8c2f12b7753f3bf6
humwawe <wawe8963098@163.com>
2020-04-18 20:09:29 +0800

    lower&upper bound

--- src/main/java/template/search/BinarySearch.java
+++ src/main/java/template/search/BinarySearch.java
@@ -1,5 +1,7 @@
 package template.search;
 
+import java.util.*;
+
 /**
  * @author hum
  */
@@ -93,4 +95,52 @@ public class BinarySearch {
         return l;
     }
 
+    // [l,r] 闭区间，从左往右返回第一个大于等于t的位置，末尾最多到r
+    int lowerBound(int[] a, int t, int l, int r) {
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (a[mid] >= t) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    // [l,r] 闭区间，从右往左返回第一个小于等于t的位置，最小最多到0
+    int upperBound(int[] a, int t, int l, int r) {
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (a[mid] <= t) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    // 返回插入位置
+    int lowerBoundArray(Integer[] arr, int t) {
+        // 自定义比较器，比较器没有返回0
+        int result = Arrays.binarySearch(arr, t, (o1, o2) -> (o1.compareTo(o2) >= 0) ? 1 : -1);
+        // 所有result均为负数，取反等价于 -result-1
+        return (result >= 0) ? result : ~result;
+    }
+
+    int upperBoundArray(Integer[] arr, int t) {
+        int result = Arrays.binarySearch(arr, t, (o1, o2) -> (o1.compareTo(o2) > 0) ? 1 : -1);
+        return (result >= 0) ? result : ~result;
+    }
+
+    int lowerBoundList(List<Integer> list, int t) {
+        int result = Collections.binarySearch(list, t, (o1, o2) -> (o1.compareTo(o2) >= 0) ? 1 : -1);
+        return (result >= 0) ? result : ~result;
+    }
+
+    int upperBoundList(List<Integer> list, int t) {
+        int result = Collections.binarySearch(list, t, (o1, o2) -> (o1.compareTo(o2) > 0) ? 1 : -1);
+        return (result >= 0) ? result : ~result;
+    }
 }
