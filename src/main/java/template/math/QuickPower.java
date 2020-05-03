1bd965c5ebf7f236b4d5f0145ae6a3d182a66b7d
humwawe <wawe8963098@163.com>
2020-05-03 18:12:59 +0800

    exgcd quick power

---
+++ src/main/java/template/math/QuickPower.java
@@ -0,0 +1,18 @@
+package template.math;
+
+/**
+ * @author hum
+ */
+public class QuickPower {
+    int qp(int m, int k, int p) {
+        long res = 1 % p, t = m;
+        while (k > 0) {
+            if ((k & 1) == 1) {
+                res = res * t % p;
+            }
+            t = t * t % p;
+            k >>= 1;
+        }
+        return (int) res % p;
+    }
+}
