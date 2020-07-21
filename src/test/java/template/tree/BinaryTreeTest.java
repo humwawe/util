56ae241e9945ead36deb37938a1d42f518aed1f8
humwawe <wawe8963098@163.com>
2020-07-22 00:08:43 +0800

    binary tree codec

--- src/test/java/template/tree/BinaryTreeTest.java
+++ src/test/java/template/tree/BinaryTreeTest.java
@@ -11,10 +11,10 @@ class BinaryTreeTest {
         System.out.println(binaryTree.preAndPostBuildTree(new int[]{1, 2}, new int[]{2, 1}));
         TreeNode treeNode = binaryTree.deserializeBfs("5,4,7,3,#,2,#,-1,#,9");
         System.out.println(binaryTree.serializeBfs(treeNode));
-        TreeNode treeNode1 = binaryTree.deserializePre("1,2,#,4,#,#,3");
+        TreeNode treeNode1 = binaryTree.deserializePre("");
         StringBuilder sb = new StringBuilder();
         binaryTree.serializePre(treeNode1, sb);
-        binaryTree.serializePreHelper(sb);
+        binaryTree.removeLastNull(sb);
         System.out.println(sb.toString());
 
     }
