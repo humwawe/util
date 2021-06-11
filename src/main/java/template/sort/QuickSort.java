e5bba6d249ee0f4c02b82b576d38e39d3420189b
humwawe <wawe8963098@163.com>
2021-06-11 21:36:21 +0800

    radixSort2

--- src/main/java/template/sort/QuickSort.java
+++ src/main/java/template/sort/QuickSort.java
@@ -32,4 +32,37 @@ public class QuickSort {
         quickSort(q, j + 1, r);
     }
 
+    public int[] radixSort2(int[] a) {
+        int n = a.length;
+        int[] c0 = new int[0x101];
+        int[] c1 = new int[0x101];
+        int[] c2 = new int[0x101];
+        int[] c3 = new int[0x101];
+        for (int v : a) {
+            c0[(v & 0xff) + 1]++;
+            c1[(v >>> 8 & 0xff) + 1]++;
+            c2[(v >>> 16 & 0xff) + 1]++;
+            c3[(v >>> 24 ^ 0x80) + 1]++;
+        }
+        for (int i = 0; i < 0xff; i++) {
+            c0[i + 1] += c0[i];
+            c1[i + 1] += c1[i];
+            c2[i + 1] += c2[i];
+            c3[i + 1] += c3[i];
+        }
+        int[] t = new int[n];
+        for (int v : a) {
+            t[c0[v & 0xff]++] = v;
+        }
+        for (int v : t) {
+            a[c1[v >>> 8 & 0xff]++] = v;
+        }
+        for (int v : a) {
+            t[c2[v >>> 16 & 0xff]++] = v;
+        }
+        for (int v : t) {
+            a[c3[v >>> 24 ^ 0x80]++] = v;
+        }
+        return a;
+    }
 }
--- src/test/java/template/sort/QuickSortTest.java
+++ src/test/java/template/sort/QuickSortTest.java
@@ -2,16 +2,27 @@ package template.sort;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
 
 class QuickSortTest {
 
     @Test
     void quickSort() {
         QuickSort quickSort = new QuickSort();
-        int[] q = new int[]{1, 1, 1};
+        int[] q = new int[]{1, 2, 1, 3, -1, -12, -9};
         int l = 0;
         int r = q.length - 1;
         quickSort.quickSort(q, l, r);
+        System.out.println(Arrays.toString(q));
+    }
+
+    @Test
+    void radioSort() {
+        QuickSort quickSort = new QuickSort();
+        int[] q = new int[]{1, 2, 1, 3, -1, -12, -9, 10};
+        int l = 0;
+        int r = q.length - 1;
+        quickSort.radixSort2(q);
+        System.out.println(Arrays.toString(q));
     }
 }
