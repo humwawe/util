eae2d63464443b837dd0f59afe402c744b7bcd17
humwawe <wawe8963098@163.com>
2021-06-20 20:51:38 +0800

    radixSort

--- src/main/java/template/sort/QuickSort.java
+++ src/main/java/template/sort/QuickSort.java
@@ -32,106 +32,111 @@ public class QuickSort {
         quickSort(q, j + 1, r);
     }
 
-    public static long[] radixSort(long[] f) {
-        return radixSort(f, f.length);
-    }
-
-    public static long[] radixSort(long[] f, int n) {
-        long[] to = new long[n];
-        {
-            int[] b = new int[65537];
-            for (int i = 0; i < n; i++) {
-                b[1 + (int) (f[i] & 0xffff)]++;
-            }
-            for (int i = 1; i <= 65536; i++) {
-                b[i] += b[i - 1];
-            }
-            for (int i = 0; i < n; i++) {
-                to[b[(int) (f[i] & 0xffff)]++] = f[i];
-            }
-            long[] d = f;
-            f = to;
-            to = d;
-        }
-        {
-            int[] b = new int[65537];
-            for (int i = 0; i < n; i++) {
-                b[1 + (int) (f[i] >>> 16 & 0xffff)]++;
-            }
-            for (int i = 1; i <= 65536; i++) {
-                b[i] += b[i - 1];
-            }
-            for (int i = 0; i < n; i++) {
-                to[b[(int) (f[i] >>> 16 & 0xffff)]++] = f[i];
-            }
-            long[] d = f;
-            f = to;
-            to = d;
-        }
-        {
-            int[] b = new int[65537];
-            for (int i = 0; i < n; i++) {
-                b[1 + (int) (f[i] >>> 32 & 0xffff)]++;
-            }
-            for (int i = 1; i <= 65536; i++) {
-                b[i] += b[i - 1];
-            }
-            for (int i = 0; i < n; i++) {
-                to[b[(int) (f[i] >>> 32 & 0xffff)]++] = f[i];
-            }
-            long[] d = f;
-            f = to;
-            to = d;
-        }
-        {
-            int[] b = new int[65537];
-            for (int i = 0; i < n; i++) {
-                b[1 + (int) (f[i] >>> 48 & 0xffff)]++;
-            }
-            for (int i = 1; i <= 65536; i++) {
-                b[i] += b[i - 1];
-            }
-            for (int i = 0; i < n; i++) {
-                to[b[(int) (f[i] >>> 48 & 0xffff)]++] = f[i];
-            }
-            long[] d = f;
-            f = to;
-            to = d;
-        }
-        return f;
-    }
-
-    public int[] radixSort2(int[] a) {
-        int n = a.length;
+    public void longRadixSort(long[] a) {
+        int fr = 0;
+        int to = a.length;
         int[] c0 = new int[0x101];
         int[] c1 = new int[0x101];
         int[] c2 = new int[0x101];
         int[] c3 = new int[0x101];
-        for (int v : a) {
+        int[] c4 = new int[0x101];
+        int[] c5 = new int[0x101];
+        int[] c6 = new int[0x101];
+        int[] c7 = new int[0x101];
+        c0[0] = c1[0] = c2[0] = c3[0] = c4[0] = c5[0] = c6[0] = c7[0] = fr;
+        for (int i = fr; i < to; i++) {
+            long v = a[i];
+            c0[(int) (v & 0xff) + 1]++;
+            c1[(int) (v >>> 8 & 0xff) + 1]++;
+            c2[(int) (v >>> 16 & 0xff) + 1]++;
+            c3[(int) (v >>> 24 & 0xff) + 1]++;
+            c4[(int) (v >>> 32 & 0xff) + 1]++;
+            c5[(int) (v >>> 40 & 0xff) + 1]++;
+            c6[(int) (v >>> 48 & 0xff) + 1]++;
+            c7[(int) (v >>> 56 ^ 0x80) + 1]++;
+        }
+        for (int i = 0; i < 0x100; i++) {
+            c0[i + 1] += c0[i];
+            c1[i + 1] += c1[i];
+            c2[i + 1] += c2[i];
+            c3[i + 1] += c3[i];
+            c4[i + 1] += c4[i];
+            c5[i + 1] += c5[i];
+            c6[i + 1] += c6[i];
+            c7[i + 1] += c7[i];
+        }
+        long[] b = new long[a.length];
+        for (int i = fr; i < to; i++) {
+            long v = a[i];
+            b[c0[(int) (v & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = b[i];
+            a[c1[(int) (v >>> 8 & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = a[i];
+            b[c2[(int) (v >>> 16 & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = b[i];
+            a[c3[(int) (v >>> 24 & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = a[i];
+            b[c4[(int) (v >>> 32 & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = b[i];
+            a[c5[(int) (v >>> 40 & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = a[i];
+            b[c6[(int) (v >>> 48 & 0xff)]++] = v;
+        }
+        for (int i = fr; i < to; i++) {
+            long v = b[i];
+            a[c7[(int) (v >>> 56 ^ 0x80)]++] = v;
+        }
+    }
+
+    public void intRadixSort(int[] a) {
+        int fr = 0;
+        int to = a.length;
+        int[] c0 = new int[0x101];
+        int[] c1 = new int[0x101];
+        int[] c2 = new int[0x101];
+        int[] c3 = new int[0x101];
+        c0[0] = c1[0] = c2[0] = c3[0] = fr;
+        for (int i = fr; i < to; i++) {
+            int v = a[i];
             c0[(v & 0xff) + 1]++;
             c1[(v >>> 8 & 0xff) + 1]++;
             c2[(v >>> 16 & 0xff) + 1]++;
             c3[(v >>> 24 ^ 0x80) + 1]++;
         }
-        for (int i = 0; i < 0xff; i++) {
+        for (int i = 0; i < 0x100; i++) {
             c0[i + 1] += c0[i];
             c1[i + 1] += c1[i];
             c2[i + 1] += c2[i];
             c3[i + 1] += c3[i];
         }
-        int[] t = new int[n];
-        for (int v : a) {
-            t[c0[v & 0xff]++] = v;
+        int[] b = new int[a.length];
+        for (int i = fr; i < to; i++) {
+            int v = a[i];
+            b[c0[v & 0xff]++] = v;
         }
-        for (int v : t) {
+        for (int i = fr; i < to; i++) {
+            int v = b[i];
             a[c1[v >>> 8 & 0xff]++] = v;
         }
-        for (int v : a) {
-            t[c2[v >>> 16 & 0xff]++] = v;
+        for (int i = fr; i < to; i++) {
+            int v = a[i];
+            b[c2[v >>> 16 & 0xff]++] = v;
         }
-        for (int v : t) {
+        for (int i = fr; i < to; i++) {
+            int v = b[i];
             a[c3[v >>> 24 ^ 0x80]++] = v;
         }
-        return a;
     }
 }
--- src/test/java/template/sort/QuickSortTest.java
+++ src/test/java/template/sort/QuickSortTest.java
@@ -20,9 +20,10 @@ class QuickSortTest {
     void radioSort() {
         QuickSort quickSort = new QuickSort();
         int[] q = new int[]{1, 2, 1, 3, -1, -12, -9, 10};
-        int l = 0;
-        int r = q.length - 1;
-        quickSort.radixSort2(q);
+        quickSort.intRadixSort(q);
         System.out.println(Arrays.toString(q));
+        long[] q2 = new long[]{1, 2, 1, 3, -1, -13414, -9, -123423, -2345234, -23452, -2345245, 10000000000000L};
+        quickSort.longRadixSort(q2);
+        System.out.println(Arrays.toString(q2));
     }
 }
