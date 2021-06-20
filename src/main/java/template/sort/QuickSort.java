6a832b755716b923aa5dd9528291fbd4ae99b24c
humwawe <wawe8963098@163.com>
2021-06-20 20:29:09 +0800

    radixSort for long

--- src/main/java/template/sort/QuickSort.java
+++ src/main/java/template/sort/QuickSort.java
@@ -32,6 +32,75 @@ public class QuickSort {
         quickSort(q, j + 1, r);
     }
 
+    public static long[] radixSort(long[] f) {
+        return radixSort(f, f.length);
+    }
+
+    public static long[] radixSort(long[] f, int n) {
+        long[] to = new long[n];
+        {
+            int[] b = new int[65537];
+            for (int i = 0; i < n; i++) {
+                b[1 + (int) (f[i] & 0xffff)]++;
+            }
+            for (int i = 1; i <= 65536; i++) {
+                b[i] += b[i - 1];
+            }
+            for (int i = 0; i < n; i++) {
+                to[b[(int) (f[i] & 0xffff)]++] = f[i];
+            }
+            long[] d = f;
+            f = to;
+            to = d;
+        }
+        {
+            int[] b = new int[65537];
+            for (int i = 0; i < n; i++) {
+                b[1 + (int) (f[i] >>> 16 & 0xffff)]++;
+            }
+            for (int i = 1; i <= 65536; i++) {
+                b[i] += b[i - 1];
+            }
+            for (int i = 0; i < n; i++) {
+                to[b[(int) (f[i] >>> 16 & 0xffff)]++] = f[i];
+            }
+            long[] d = f;
+            f = to;
+            to = d;
+        }
+        {
+            int[] b = new int[65537];
+            for (int i = 0; i < n; i++) {
+                b[1 + (int) (f[i] >>> 32 & 0xffff)]++;
+            }
+            for (int i = 1; i <= 65536; i++) {
+                b[i] += b[i - 1];
+            }
+            for (int i = 0; i < n; i++) {
+                to[b[(int) (f[i] >>> 32 & 0xffff)]++] = f[i];
+            }
+            long[] d = f;
+            f = to;
+            to = d;
+        }
+        {
+            int[] b = new int[65537];
+            for (int i = 0; i < n; i++) {
+                b[1 + (int) (f[i] >>> 48 & 0xffff)]++;
+            }
+            for (int i = 1; i <= 65536; i++) {
+                b[i] += b[i - 1];
+            }
+            for (int i = 0; i < n; i++) {
+                to[b[(int) (f[i] >>> 48 & 0xffff)]++] = f[i];
+            }
+            long[] d = f;
+            f = to;
+            to = d;
+        }
+        return f;
+    }
+
     public int[] radixSort2(int[] a) {
         int n = a.length;
         int[] c0 = new int[0x101];
