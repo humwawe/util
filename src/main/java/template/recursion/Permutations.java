1adf1b7f37dfb844708f3bf559edc76412d90e22
humwawe <wawe8963098@163.com>
2021-07-26 23:09:08 +0800

    nextPermutation

--- src/main/java/template/recursion/Permutations.java
+++ src/main/java/template/recursion/Permutations.java
@@ -33,6 +33,28 @@ public class Permutations {
         }
     }
 
+    int n = 10;
+    int[] nums = new int[n];
+
+    // 枚举nums的全排列，无顺序
+    private void helper1(int cur) {
+        if (cur == n) {
+            // nums为一个排列
+            return;
+        }
+        for (int i = cur; i < n; i++) {
+            swap(cur, i);
+            helper1(cur + 1);
+            swap(cur, i);
+        }
+    }
+
+    private void swap(int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
     /**
      * get nums permutations
      * record last used num
@@ -62,5 +84,30 @@ public class Permutations {
         }
     }
 
-
+    // nums的下一个排列，len表示nums的长度
+    public void nextPermutation(int[] nums, int len) {
+        int i = len - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            for (int j = len - 1; j > i; j--) {
+                if (nums[j] > nums[i]) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+        }
+        int left = i + 1;
+        int right = len - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
