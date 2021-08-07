3ca3419cb4f377be179d40bb114bfb83e753fef6
humwawe <wawe8963098@163.com>
2021-08-08 00:17:22 +0800

    manacher

--- src/main/java/template/string/Manacher.java
+++ src/main/java/template/string/Manacher.java
@@ -10,12 +10,12 @@ public class Manacher {
 
     /**
      * abaaba
-     * i: 0 1 2 3 4 5 6 7 8 9 10 11 12 13
-     * ma[i]: $ # a # b # a # a # b # a #
-     * mp[i]: 1 1 2 1 4 1 2 7 2 1 4 1 2 1
+     * i:     0 1 2 3 4 5 6 7 8 9 10 11 12 13
+     * ma[i]: $ # a # b # a # a # b  #  a  #
+     * mp[i]: 1 1 2 1 4 1 2 7 2 1 4  1  2  1
      * 遍历 2*len(str)+2 mp[i]-1 为从i向左右拓展的结果
      */
-    void Manacher(char s[], int len) {
+    void manacher(char s[], int len) {
         int l = 0;
         ma[l++] = '$';
         ma[l++] = '#';
@@ -23,11 +23,10 @@ public class Manacher {
             ma[l++] = s[i];
             ma[l++] = '#';
         }
-        ma[l] = 0;
         int mx = 0, id = 0;
         for (int i = 0; i < l; i++) {
             mp[i] = mx > i ? Math.min(mp[2 * id - i], mx - i) : 1;
-            while (ma[i + mp[i]] == ma[i - mp[i]]) {
+            while (i + mp[i] < l && i - mp[i] >= 0 && ma[i + mp[i]] == ma[i - mp[i]]) {
                 mp[i]++;
             }
             if (i + mp[i] > mx) {
