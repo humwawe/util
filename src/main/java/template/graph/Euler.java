31d891119f733144073f5ec88862f9b7e2729be2
humwawe <wawe8963098@163.com>
2021-09-26 21:33:11 +0800

    Euler

--- src/main/java/template/graph/Euler.java
+++ src/main/java/template/graph/Euler.java
@@ -1,9 +1,6 @@
 package template.graph;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author hum
  */
@@ -13,32 +10,66 @@ public class Euler {
     int[] h = new int[N];
     int[] e = new int[M];
     int[] ne = new int[M];
-    boolean[] f = new boolean[M];
-    int idx;
+    boolean[] vis = new boolean[M];
     // 保存欧拉回路结果
-    List<Integer> list = new ArrayList<>();
+    int[] res = new int[M];
+    int t = 0;
 
+    // 普通写法，一个点可能被遍历多次，而每次都扫描了它相邻的边
     void dfs(int u) {
         for (int i = h[u]; i != -1; i = ne[i]) {
             int j = e[i];
-            if (f[i]) {
-                f[i] = false;
-                f[i ^ 1] = false;
+            if (!vis[i]) {
+                vis[i] = vis[i ^ 1] = true;
                 dfs(j);
-                list.add(j);
+            }
+        }
+        // 入栈
+        res[t++] = u;
+    }
+
+    // 优化，每次访问后，及时修改head指向下一个
+    void dfs2(int u) {
+        while (true) {
+            int i = h[u];
+            while (i != -1 && vis[i]) {
+                i = ne[i];
+            }
+            if (i != -1) {
+                h[u] = ne[i];
+                vis[i] = vis[i ^ 1] = true;
+                int j = e[i];
+                dfs2(j);
+            } else {
+                break;
+            }
+        }
+        res[t++] = u;
+    }
+
+    // 非递归防止爆栈
+    int[] stack = new int[M];
+    int top;
+
+    void euler() {
+        stack[++top] = 1;
+        while (top > 0) {
+            int u = stack[top];
+            int i = h[u];
+            while (i != -1 && vis[i]) {
+                i = ne[i];
+            }
+            if (i != -1) {
+                h[u] = ne[i];
+                vis[i] = vis[i ^ 1] = true;
+                stack[++top] = e[i];
+            } else {
+                // u所有边已访问，模拟回溯，并记录答案
+                res[t++] = u;
+                top--;
             }
         }
     }
 
-    void add(int a, int b) {
-        e[idx] = b;
-        ne[idx] = h[a];
-        f[idx] = true;
-        h[a] = idx++;
-        e[idx] = a;
-        ne[idx] = h[b];
-        f[idx] = true;
-        h[b] = idx++;
-    }
 
 }
