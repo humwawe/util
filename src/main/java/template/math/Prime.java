5fa926c16f0e70eee7bcbcf07dccdac4be7c0ef6
humwawe <wawe8963098@163.com>
2021-11-26 23:56:38 +0800

    getPrimes线性筛

--- src/main/java/template/math/Prime.java
+++ src/main/java/template/math/Prime.java
@@ -70,6 +70,46 @@ public class Prime {
         }
     }
 
+    //线性筛法，每个合数被最小的质因子筛掉
+    void getPrimes3(int n) {
+        int[] primes = new int[n + 1];
+        // sieve存每个数的最小质因子
+        int[] sieve = new int[n + 1];
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            if (sieve[i] == 0) {
+                sieve[i] = i;
+                primes[cnt++] = i;
+            }
+            // 给当前i乘一个质因子
+            for (int j = 0; j < cnt; j++) {
+                // 超出范围，或者当前质因数已经不是最小质因子
+                if (primes[j] > n / i || primes[j] > sieve[i]) {
+                    break;
+                }
+                sieve[i * primes[j]] = primes[j];
+            }
+        }
+    }
+
+    List<Integer> prime = new ArrayList<>();
+    int N = 105;
+    int[] sieve = new int[N + 1];
+
+    // sieve存每个数的最小质因子，也是线性筛
+    void sieve() {
+        sieve[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            if (sieve[i] == 0) {
+                sieve[i] = i;
+                prime.add(i);
+            }
+            for (int j = 0; j < prime.size() && prime.get(j) <= sieve[i] && i * prime.get(j) <= N; j++) {
+                sieve[i * prime.get(j)] = prime.get(j);
+            }
+        }
+    }
+
     // 素数分解
     void divide(int x) {
         for (int i = 2; i <= x / i; i++) {
@@ -88,22 +128,5 @@ public class Prime {
         }
     }
 
-    List<Integer> prime = new ArrayList<>();
-    int N = 105;
-    int[] sieve = new int[N + 1];
-
-    // sieve存每个数的最小质因子
-    void sieve() {
-        sieve[1] = 1;
-        for (int i = 2; i <= N; i++) {
-            if (sieve[i] == 0) {
-                sieve[i] = i;
-                prime.add(i);
-            }
-            for (int j = 0; j < prime.size() && prime.get(j) <= sieve[i] && i * prime.get(j) <= N; j++) {
-                sieve[i * prime.get(j)] = prime.get(j);
-            }
-        }
-    }
 
 }
