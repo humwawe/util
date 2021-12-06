7e413eafd1738b03d0b82ce038481efbcbd05ba4
humwawe <wawe8963098@163.com>
2021-12-06 22:26:50 +0800

    Euler

--- src/main/java/template/graph/Euler.java
+++ src/main/java/template/graph/Euler.java
@@ -9,6 +9,7 @@ public class Euler {
     int M = 205;
     int[] h = new int[N];
     int[] e = new int[M];
+    int[] w = new int[M];
     int[] ne = new int[M];
     boolean[] vis = new boolean[M];
     // 保存欧拉回路结果
@@ -45,6 +46,27 @@ public class Euler {
             }
         }
         res[t++] = u;
+    }
+
+    // 用w[i]记录边序号
+    // 递归的时候传入可以使res直接记录边的序号
+    // dfs3(start, -1)，结果逆序输出即可，最后一个点为-1，输出时候跳过
+    void dfs3(int u, int pre) {
+        while (true) {
+            int i = h[u];
+            while (i != -1 && vis[i]) {
+                i = ne[i];
+            }
+            if (i != -1) {
+                h[u] = ne[i];
+                vis[i] = vis[i ^ 1] = true;
+                int j = e[i];
+                dfs3(j, w[i]);
+            } else {
+                break;
+            }
+        }
+        res[t++] = pre;
     }
 
     // 非递归防止爆栈
