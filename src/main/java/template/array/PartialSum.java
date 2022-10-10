a071b1c64e9fdf4e2690d6f45cb61fcfb1336ed2
hum <wawe8963098@163.com>
2022-10-10 17:58:15 +0800

    sos dp

--- src/main/java/template/array/PartialSum.java
+++ src/main/java/template/array/PartialSum.java
@@ -1,7 +1,5 @@
 package template.array;
 
-import java.util.Arrays;
-
 /**
  * @author hum
  */
@@ -38,28 +36,5 @@ public class PartialSum {
     return sum[x2][y2] + sum[x1 - 1][y1 - 1] - sum[x1 - 1][y2] - sum[x2][y1 - 1];
   }
 
-  // 基于 DP 计算高维前缀和
-
-
-  // d为维度，state'为第i维恰好比state少1的点
-  // for state: sum[state] = f[state];
-  // for(i = 0;i <= D;i += 1)
-  //   for 以字典序从小到大枚举 state
-  //     sum[state] += sum[state'];
-
-  // 以二进制的子集求和为例，超集可以求后缀和
-  int[] sosDp(int n, int[] a) {
-    int lim = 1 << n;
-    // 初始化
-    int[] sum = Arrays.copyOf(a, lim);
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < lim; j++) {
-        if (((j >> i) & 1) == 1) {
-          sum[j] += sum[j - (1 << i)];
-        }
-      }
-    }
-    return sum;
-  }
 
 }
