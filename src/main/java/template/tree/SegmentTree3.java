346e106fa7f95b0e87a19d5db273023b7ec4090e
hum-abc <hum@abc>
2022-10-17 11:16:35 +0800

    SegmentTree

--- src/main/java/template/tree/SegmentTree3.java
+++ src/main/java/template/tree/SegmentTree3.java
@@ -2,131 +2,94 @@ package template.tree;
 
 /**
  * @author hum
- * 动态加点
+ * 处理区间修改的问题，不使用node结构体，避免频繁new node
  */
 public class SegmentTree3 {
-  Node root;
+  int N;
+  int[] left;
+  int[] right;
+  int[] sum;
+  int[] add;
+  int[] w;
 
-  void pushUp(Node u) {
-    u.sum = u.leftChild.sum + u.rightChild.sum;
+  public SegmentTree3(int n) {
+    N = n + 5;
+    left = new int[N * 4];
+    right = new int[N * 4];
+    sum = new int[N * 4];
+    add = new int[N * 4];
+    w = new int[n];
   }
 
-  void pushDown(Node u) {
-    if (u.add != 0) {
-      int mid = u.l + u.r >> 1;
-      if (u.leftChild == null) {
-        u.leftChild = new Node(u.l, mid);
-      }
-      if (u.rightChild == null) {
-        u.rightChild = new Node(mid + 1, u.r);
-      }
-      u.leftChild.add += u.add;
-      u.leftChild.sum += (u.leftChild.r - u.leftChild.l + 1) * u.add;
-      u.rightChild.add += u.add;
-      u.rightChild.sum += (u.rightChild.r - u.rightChild.l + 1) * u.add;
-      u.add = 0;
+  void pushUp(int u) {
+    sum[u] = sum[u << 1] + sum[u << 1 | 1];
+
+  }
+
+  void pushDown(int u) {
+    if (add[u] != 0) {
+      add[u << 1] += add[u];
+      sum[u << 1] += (right[u << 1] - left[u << 1] + 1) * add[u];
+      add[u << 1 | 1] += add[u];
+      sum[u << 1 | 1] += (right[u << 1 | 1] - left[u << 1 | 1] + 1) * add[u];
+      add[u] = 0;
     }
   }
 
-  void build(int start, int end) {
-    root = new Node(start, end);
-  }
-
-  void modify(Node u, int l, int r, int d) {
-    if (u.l >= l && u.r <= r) {
-      u.sum += (u.r - u.l + 1) * d;
-      u.add += d;
+  // 从u开始，构建[l,r]的树，w[i]存每个节点的值(从l到r)
+  void build(int u, int l, int r) {
+    if (l == r) {
+      left[u] = l;
+      right[u] = r;
+      sum[u] = w[r];
       return;
     }
-    int mid = u.l + u.r >> 1;
-    if (u.leftChild == null) {
-      u.leftChild = new Node(u.l, mid);
+    left[u] = l;
+    right[u] = r;
+    int mid = l + r >> 1;
+    build(u << 1, l, mid);
+    build(u << 1 | 1, mid + 1, r);
+    pushUp(u);
+  }
+
+  // 从u开始查找
+  int query(int u, int l, int r) {
+    // 已经完全在[l,r]中了
+    if (left[u] >= l && right[u] <= r) {
+      return sum[u];
     }
-    if (u.rightChild == null) {
-      u.rightChild = new Node(mid + 1, u.r);
+    pushDown(u);
+    int mid = left[u] + right[u] >> 1;
+    if (r <= mid) {
+      return query(u << 1, l, r);
+    } else if (l > mid) {
+      return query(u << 1 | 1, l, r);
+    } else {
+      return query(u << 1, l, r) + query(u << 1 | 1, l, r);
+    }
+
+  }
+
+  void modify(int u, int l, int r, int d) {
+    if (left[u] >= l && right[u] <= r) {
+      sum[u] += (right[u] - left[u] + 1) * d;
+      add[u] += d;
+      return;
     }
     // 分裂
     pushDown(u);
 
+    int mid = left[u] + right[u] >> 1;
     if (r <= mid) {
-      modify(u.leftChild, l, r, d);
+      modify(u << 1, l, r, d);
     } else if (l > mid) {
-      modify(u.rightChild, l, r, d);
+      modify(u << 1 | 1, l, r, d);
     } else {
-      modify(u.leftChild, l, r, d);
-      modify(u.rightChild, l, r, d);
+      modify(u << 1, l, r, d);
+      modify(u << 1 | 1, l, r, d);
     }
 
     pushUp(u);
   }
 
-  // 从u开始查找
-  int query(Node u, int l, int r) {
-    if (u == null) {
-      return 0;
-    }
-    if (u.l >= l && u.r <= r) {
-      return u.sum;
-    }
-
-    pushDown(u);
-    int mid = u.l + u.r >> 1;
-    if (r <= mid) {
-      return query(u.leftChild, l, r);
-    } else if (l > mid) {
-      return query(u.rightChild, l, r);
-    } else {
-      return query(u.leftChild, l, r) + query(u.rightChild, l, r);
-    }
-
-  }
-
-  // 合并两个线段树到p上，将对应值相加
-  // s1.root = s1.merge(s1.root, s2.root);
-  Node merge(Node p, Node q) {
-    if (p == null) {
-      return q;
-    }
-    if (q == null) {
-      return p;
-    }
-
-    pushDown(p);
-    pushDown(q);
-
-    // 合并到最后
-    if (p.l == p.r) {
-      p.sum += q.sum;
-      //  p.add += q.add;
-      return p;
-    }
-    p.leftChild = merge(p.leftChild, q.leftChild);
-    p.rightChild = merge(p.rightChild, q.rightChild);
-
-    pushUp(p);
-
-    return p;
-  }
-
-  class Node {
-    int l;
-    int r;
-    Node leftChild;
-    Node rightChild;
-    int sum;
-    // 懒标记，给儿子加上add，不包括自己
-    int add;
-
-    public Node(int l, int r) {
-      this.l = l;
-      this.r = r;
-    }
-
-    public Node(int l, int r, int sum, int add) {
-      this.l = l;
-      this.r = r;
-      this.sum = sum;
-      this.add = add;
-    }
-  }
 }
