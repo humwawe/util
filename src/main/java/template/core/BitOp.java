5d5e3b095c35b03090bfaace04e6f3054569dc83
humwawe <wawe8963098@163.com>
2022-12-20 21:38:45 +0800

    BitOp

--- src/main/java/template/core/BitOp.java
+++ src/main/java/template/core/BitOp.java
@@ -7,18 +7,18 @@ public class BitOp {
 
   // x 的最后一个 1 代表的值
   // 1010 -> 10 -> 2
-  public int lowbit(int x) {
+  public static int lowbit(int x) {
     return x & -x;
     //  return Integer.lowestOneBit(x);
   }
 
   // x 的第一个 1 代表的值
   // 1010 -> 1000 -> 8
-  public int highbit(int x) {
+  public static int highbit(int x) {
     return Integer.highestOneBit(x);
   }
 
-  public boolean isPow2(int x) {
+  public static boolean isPow2(int x) {
     return x > 0 && x == lowbit(x);
   }
 
