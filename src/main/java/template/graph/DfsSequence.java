9564202a33b2b0c1670a49a5748b7c6e32f0a932
humwawe <wawe8963098@163.com>
2023-04-02 20:57:24 +0800

    DfsSequence

--- src/main/java/template/graph/DfsSequence.java
+++ src/main/java/template/graph/DfsSequence.java
@@ -35,51 +35,5 @@ public class DfsSequence {
     return in[a] < in[b] && out[a] >= out[b];
   }
 
-  int[] dfn = new int[N];
-  // 根据点标号如果已经有0，考虑初始化为-1
-  int[] fa = new int[N];
-  int timestamp = 0;
-  // 存上环的点
-  int[] s = new int[N];
-  // 记录环上边的权值
-  // int[] edge = new int[N];
-  int p = 0;
-
-  // to fix bug
-  // 无向图dfs找环
-  // 标定时间戳，如果访问到已经访问的点，且不是父节点，且时间戳大于当前的说明有环
-  void dfsCycle(int u) {
-    dfn[u] = ++timestamp;
-    for (int i = h[u]; i != -1; i = ne[i]) {
-      int j = e[i];
-      if (dfn[j] == 0) {
-        // 记录边的信息
-        fa[j] = i;
-        dfsCycle(j);
-      }
-      // 碰到环，取等于可以找自环
-      //（add(x,x),add(x,x)）遍历u时，会有两条j=u，自环可能会算两边
-      else if ((i ^ 1) != fa[u] && dfn[j] >= dfn[u]) {
-        getCycle(u, j, w[i]);
-      }
-    }
-  }
-
-  void getCycle(int u, int j, int z) {
-    // edge[0] = z;
-    while (j != u) {
-      s[p++] = j;
-      // edge[p] = w[fa[j]];
-      j = e[fa[j] ^ 1];
-    }
-    s[p++] = u;
-  }
-
-  void add(int a, int b, int c) {
-    e[idx] = b;
-    w[idx] = c;
-    ne[idx] = h[a];
-    h[a] = idx++;
-  }
 
 }
