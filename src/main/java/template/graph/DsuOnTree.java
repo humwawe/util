bdfcde0231e4b3385462a5957da55cb8c7b4ac54
hum <wawe8963098@163.com>
2023-07-12 15:56:41 +0800

    DsuOnTree

--- src/main/java/template/graph/DsuOnTree.java
+++ src/main/java/template/graph/DsuOnTree.java
@@ -45,9 +45,9 @@ public class DsuOnTree {
     out[u] = tot;
   }
 
-  // dfsSovle(1,1,false)
+  // dfsSolve(1,1,false)
   // keep 表示是否保留信息
-  void dfsSovle(int u, int p, boolean keep) {
+  void dfsSolve(int u, int p, boolean keep) {
 
     // 先递归算轻儿子
     for (int i = h[u]; i != -1; i = ne[i]) {
@@ -55,12 +55,12 @@ public class DsuOnTree {
       if (j == p || j == hs[u]) {
         continue;
       }
-      dfsSovle(j, u, false);
+      dfsSolve(j, u, false);
     }
 
     // 再算重儿子
     if (hs[u] != -1) {
-      dfsSovle(hs[u], u, true);
+      dfsSolve(hs[u], u, true);
     }
 
     for (int i = h[u]; i != -1; i = ne[i]) {
